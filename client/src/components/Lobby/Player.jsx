21e0572ffb7de3517004a2c7e0930cfe72cf9756
Michael Tu <michaeltu12345@gmail.com>
2023-01-28 17:53:01 -0500

    circles but for real this time

--- client/src/components/Lobby/Player.jsx
+++ client/src/components/Lobby/Player.jsx
@@ -1,5 +1,10 @@
+import "./Player.css"
+
 export default function Player({ name, uuid }) {
     return (
-        <h4>{name}: {uuid}</h4>
+        <div className="player-container">
+            <div className="player"></div>
+            <h4>{name}: {uuid}</h4>
+        </div>
     );
 }
